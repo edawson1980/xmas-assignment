3e3dbf84bcea998b80a921157550e824d460cc75
Erin <erinedawson@gmail.com>
2019-11-29 20:09:46 -0700

    write function to fill empty array to 6

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-
-
-
-
+let sibs = [];
+let randoms = 6;
 
 function assignment(){
+
+  for(let i = 0; i < randoms.length; i++){
+    sibs.push(i);
+  }
 
  let random = Math.floor((Math.random()*6)+1);
 
